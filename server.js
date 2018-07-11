04591f83203a0692bed897d5e8bae9f2b4dda5f0
Erik Withak <ErikAanestad@gmail.com>
2018-07-11 10:20:50 -0700

    Using 'Request' instead of 'HTTP'

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 
-const http = require('https');
+const request = require('request');
 const sp = require('serialport');
+
+const URL = 'https://streamlabs.com/api/donations?access_token=';
+const TOKEN = '40EF01F0CC32BC680005';
 
 const UPDATE_TIME = 1000;
 let donationsTime = ['0000-00-00 00:00:00'];
+let donations = 0;
 
-const serialPort = new sp('/dev/ttyUSB0', {
-  baudRate : 9600
-}, (err) => console.log('an error occurred', err));
+const serialPort = new sp('/dev/ttyUSB0', () => null);
 serialPort.open(() => console.log('Shock therapy open for business.'));
 
 function shockTime() {
@@ -15,18 +17,22 @@ function shockTime() {
 }
 
 function updateDonations() {
-  http.get(`https://streamlabs.com/api/donations?access_token=40EF01F0CC32BC680005&limit=10`, (res) => {
-    res.on('data', (chunk) => {
-      chunk = JSON.parse(chunk);
-      chunk = chunk.donations.filter(a => a.created_at > donationsTime[donationsTime.length - 1]).map(a => a.created_at);
-      if(chunk.length > 0) {
-        console.log('A new donation(s) has been made!');
-        donationsTime = chunk;
-        shockTime();
-      }
-    });
-  }).on('error', (e) => {
-    console.error(`Got error: ${e.message}`);
+  request(`${URL}${TOKEN}`, function (err, res, data) {
+    if(err) {
+      console.log('error:', err);
+      return;
+    }
+    if(res.statusCode !== 200) {
+      console.log('statusCode:', res && res.statusCode);
+      return;
+    }
+    let donations = JSON.parse(data);
+    donations = donations.donations.filter(a => a.created_at > donationsTime[donationsTime.length - 1]).map(b => b.created_at);
+    if(donations.length > 0) {
+            console.log('A new donation(s) has been made!');
+            donationsTime = donations[donations.length -1];
+            shockTime();
+    }
   });
   setTimeout(updateDonations, UPDATE_TIME);
 }
